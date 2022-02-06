868a1238b59cedfb364bdc2fe5edd76970bc2403
RomanBobkovTKI <bob0504@mail.ru>
2022-02-06 13:50:48 +0300

    add new function

--- test_abs_project.py
+++ test_abs_project.py
@@ -11,11 +11,15 @@ from selenium.webdriver.support import expected_conditions as EC
 def test_abs1():
     assert abs(-42) == 42, "Should be absolute value of a number"
 
+def test_abs2():
+    assert abs(-42) == -42, "Should be absolute value of a number"
+
 if __name__ == "__main__":
 
     try:
         test_abs1()
-        print("All tests passed!")
+        test_abs2()
+        print("Everything passed")
 
     
     finally:
